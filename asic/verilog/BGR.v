66988209a1efc6d3522b5590f529271b4d6a5396
Nandor <nandor@hep.upenn.edu>
2025-12-05 14:05:46 -0500

    -ncd

---
+++ asic/verilog/BGR.v
@@ -0,0 +1,11 @@
+// verilog stub required for genus to not remove physical-only macros/pads -ncd 
+`timescale 1ns / 1ns 
+module BGR ( vbg, vdda_2p5, vssa_2p5 );
+
+inout  vbg, vdda_2p5, vssa_2p5;
+
+endmodule // BGR
+
+
+
+
