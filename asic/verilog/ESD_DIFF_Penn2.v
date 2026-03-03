3a67456e179871b78090e5b1712814a28dbd8fac
Paul T. Keener <keener@hep.upenn.edu>
2026-03-03 14:30:24 -0500

    Add missing pad part and fix permissions

--- asic/verilog/ESD_DIFF_Penn2.v
+++ asic/verilog/ESD_DIFF_Penn2.v
@@ -1,9 +1,11 @@
 // verilog stub required for genus to not remove physical-only macros/pads -ncd 
 `celldefine
-module PDB1A (AIO, TAVDD, TACVDD, VSS);
-   inout wire AIO;
-   inout wire TAVDD, TACVDD, VSS;
-   
+module ESD_DIFF_Penn2 (A, P, N, DifRef, TACVDD, VSS);
+   inout A;
+   inout P, N;
+   inout DifRef;
+   inout TACVDD, VSS;
+
 
    // Behavioral placeholder ? no internal modeling
  //  tran (AIO, AIO); // THIS causes genus to uniquify! -ncd 2025
