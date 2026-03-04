0c6e498f345c5900a5ffc1687f20d56a1842f00f
Paul T. Keener <keener@hep.upenn.edu>
2026-03-04 12:38:51 -0500

    Script for running the analog_if testbench

---
+++ asic/verilog/analog_if/run_ai_tb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# Run the script plan to bring up the GUI or
+#   run as "env GUI=0 ./run_ai_tb.sh" to disable the GUI
+#
+
+if [ "x$GUI" = "x" ] ; then
+    GUI=1
+fi
+
+if [ $GUI = 1 ] ; then
+    GUI_OPT="+gui"
+else
+    GUI_OPT=""
+fi
+
+xrun $GUI_OPT -access +rw tb_analog_if.v analog_if.v cmp_sync.v \
+     cmp_latch.v clk_cnter.v
