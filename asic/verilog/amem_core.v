e02fef70929ee0c7f1136c69bfc2d086f8d62d90
anikolica <13853168+anikolica@users.noreply.github.com>
2026-03-11 13:38:19 -0400

    Shell interface between APP and FPGA control of analog memory sampling

--- asic/verilog/amem_core.v
+++ asic/verilog/amem_core.v
@@ -2,91 +2,42 @@
  * Digital interface to analog memory cells, APP core logic, and outside
  * world.
  *
+ * 1. APP updates amem_empty/full and event_mux_o
+ * 2. FPGA asserts read_next_i when external ADCs are ready
+ * 3. APP switches column MUX and asserts sample_ready_o when settled  
+ * 4. FPGA tells ADCs to sample, and asserts adc_done_i when conversion done  
+ * 5. Cycle repeats until amem_empty=1 and event_mux_o agrees with last read
+ * NOTE: the metadata need not be read syncronously with this interface
+ * NOTE: triggered mode can increment/decrement read pointer depending on 
+ *       which analog memory locations need to be read out
+ *
  ******************************************************************************
 */
 
-module amem_core(
-	// Signals to/from APP analog measurement circuitry
-	input wire TOT,
-	input wire ping,
-	input wire pong,
-	input wire [3:0] last_cell,
-	input wire [7:0] metadata,
-	output wire reset_pingpong,
+`timescale 100ps/1ps
+`default_nettype none
 
-	// signals to/from APP analog memory
-	output wire read_en,
-	output wire [2:0] event_mux,
+module amem_core(
+    // Intra-chip signals (controller, register map)
+    input wire [2:0] w_ptr_down_i, // next location to write
+    input wire incr_rd_ptr_i, // trigger increment read pointer
+    input wire decr_rd_prt_i, // trigger decrement read pointer
+	input wire circular_en_i, // chip reg setting to "cycle", 1 -> continuous
+
+	// Signals to/from APP analog measurement circuitry
+	input wire [7:0] WE_time_i, // TACs done on falling edge
+	output wire read_en_o, // goes to RE
+	output wire [2:0] event_mux_o, // goes to sel_TOT_event[3:1], outside world
 	
-	// signals to/from outside world
+	// Signals to/from outside world
 	input wire clk,
-	input wire resetb_full,
-	input wire adc_ready,
-	input wire adc_done,
-	input wire circular_en,
-	input wire read_next,
-	output wire amem_empty,
-	output wire amem_full
+	input wire rstb,
+	input wire adc_done_i, // FPGA done taking a sample
+	input wire read_next_i, // FPGA asking to advance to next location
+	output wire sample_ready_o, // APP indicating MUX is set
+	output wire amem_empty_o, // no new data
+	output wire amem_full_o // asserted if circular_en = 0 and 8 locations full
 );
 
-	// internal registers
-	reg [63:0] timestamp = 64'h0;
-	reg [63:0] curr_ts = 64'h0;
-	reg [2:0] column = 3'b000;
-
-	reg empty = 1'b1;
-	reg full = 1'b0;
-	assign amem_empty = empty;
-	assign amem_full = full;
-
-	reg reset_pingpong_reg = 1'b0;
-	reg read_en_reg = 1'b0;
-	reg [2:0] event_mux_reg = 3'b000;
-	assign reset_pingpong = reset_pingpong_reg;
-	assign read_en = read_en_reg;
-	assign event_mux = event_mux_reg;
-
-	reg [7:0] metadata_reg = 3'b000;
-	reg [7:0] curr_metadata = 3'b000;
-
-	// logic
-	always @ (negedge resetb_full)
-	begin
-		timestamp <= 64'h0;
-		column <= 4'h0;
-	end
-
-	always @ (posedge clk)
-	begin
-		timestamp <= timestamp + 1'b1;
-
-		metadata_reg <= metadata;
-
-		if (column == 3'b000)
-		begin
-			empty <= 1'b1;
-			full <= 1'b0;
-		end
-		if (column != 3'b000)
-		begin
-			empty <= 1'b0;
-			full <= 1'b0;
-		end
-		if (column == 3'b111)
-		begin
-			empty <= 1'b0;
-			full <= 1'b1;
-		end
-		
-	end
-
-	always @ (negedge TOT) // synchronize
-	begin
-		curr_ts <= timestamp; // Put this in FIFO
-		curr_metadata <= metadata_reg;
-		if (column != 3'b111)
-			column <= column + 1'b1;
-	end
-
-endmodule
+endmodule // amem_core
 
