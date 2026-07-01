7e2b762d69da786e4745aa0648725b3d48010991
Paul T. Keener <keener@hep.upenn.edu>
2026-07-01 16:06:12 -0400

    Updates

--- asic/verilog/analog_if/controller.sv
+++ asic/verilog/analog_if/controller.sv
@@ -17,9 +17,16 @@
 		    input wire [CLK_NBITS-1:0] cnt8_down_3_i,
 
 		    input wire [7:0] 	       WE_time_i,
-
+		    input wire 		       trigger_i,
+					      
 		    output wire [2:0] 	       w_ptr_up_o,
 		    output wire [2:0] 	       w_ptr_down_o,
+
+		    output wire 	       read_en_o,
+		    output wire [3:0] 	       event_mux_o,
+		    output wire 	       sample_ready_o,
+		    output wire 	       amem_empty_o,
+		    output wire 	       amem_full_o,
 
 		    input wire 		       clk,
 		    input wire 		       rstb
@@ -161,41 +168,41 @@
 	 // ONE valid
 	 if ( valid_down_i == 4'b0001 ) begin
 	    ts_down[w_ptr_down] <= cnt8_down_0_i;
-	    w_ptr_down <= (w_ptr_down + 3'h1) % 8;
+	    w_ptr_down <= (w_ptr_down + 3'h1) % 4'h8;
 	 end
 	 else if ( valid_down_i == 4'b0010 ) begin
 	    ts_down[w_ptr_down] <= cnt8_down_1_i;
-	    w_ptr_down <= (w_ptr_down + 3'h1) % 8;
+	    w_ptr_down <= (w_ptr_down + 3'h1) % 4'h8;
 	 end
 	 else if ( valid_down_i == 4'b0100 ) begin
 	    ts_down[w_ptr_down] <= cnt8_down_2_i;
-	    w_ptr_down <= (w_ptr_down + 3'h1) % 8;
+	    w_ptr_down <= (w_ptr_down + 3'h1) % 4'h8;
 	 end
 	 else if ( valid_down_i == 4'b1000 ) begin
 	    ts_down[w_ptr_down] <= cnt8_down_3_i;
-	    w_ptr_down <= (w_ptr_down + 3'h1) % 8;
+	    w_ptr_down <= (w_ptr_down + 3'h1) % 4'h8;
 	 end
 	 else
 	   // TWO valids
 	   if ( valid_down_i == 4'b0011 ) begin
 	      ts_down[w_ptr_down] <= cnt8_down_0_i;
 	      ts_down[w_ptr_down+1'b1] <= cnt8_down_1_i;
-	      w_ptr_down <= (w_ptr_down + 3'h2) % 8;
+	      w_ptr_down <= (w_ptr_down + 3'h2) % 4'h8;
 	   end
 	   else if ( valid_down_i == 4'b0110 ) begin
 	      ts_down[w_ptr_down] <= cnt8_down_1_i;
 	      ts_down[w_ptr_down+1'b1] <= cnt8_down_2_i;
-	      w_ptr_down <= (w_ptr_down + 3'h2) % 8;
+	      w_ptr_down <= (w_ptr_down + 3'h2) % 4'h8;
 	   end
 	   else if ( valid_down_i == 4'b1100 ) begin
 	      ts_down[w_ptr_down] <= cnt8_down_2_i;
 	      ts_down[w_ptr_down+1'b1] <= cnt8_down_3_i;
-	      w_ptr_down <= (w_ptr_down + 3'h2) % 8;
+	      w_ptr_down <= (w_ptr_down + 3'h2) % 4'h8;
 	   end
 	   else if ( valid_down_i == 4'b1001 ) begin
 	      ts_down[w_ptr_down] <= cnt8_down_3_i;
 	      ts_down[w_ptr_down+1'b1] <= cnt8_down_0_i;
-	      w_ptr_down <= (w_ptr_down + 3'h2) % 8;
+	      w_ptr_down <= (w_ptr_down + 3'h2) % 4'h8;
 	   end
 	   else
 	     // THREE valids
@@ -203,19 +210,19 @@
 		ts_down[w_ptr_down] <= cnt8_down_0_i;
 		ts_down[w_ptr_down+3'h1] <= cnt8_down_1_i;
 		ts_down[w_ptr_down+3'h2] <= cnt8_down_2_i;
-		w_ptr_down <= (w_ptr_down + 3'h3) % 8;
+		w_ptr_down <= (w_ptr_down + 3'h3) % 4'h8;
 	     end
 	     else if ( valid_down_i == 4'b1110 ) begin
 		ts_down[w_ptr_down] <= cnt8_down_1_i;
 		ts_down[w_ptr_down+3'h1] <= cnt8_down_2_i;
 		ts_down[w_ptr_down+3'h2] <= cnt8_down_3_i;
-		w_ptr_down <= (w_ptr_down + 3'h3) % 8;
+		w_ptr_down <= (w_ptr_down + 3'h3) % 4'h8;
 	     end
 	     else if ( valid_down_i == 4'b1101 ) begin
 		ts_down[w_ptr_down] <= cnt8_down_2_i;
 		ts_down[w_ptr_down+3'h1] <= cnt8_down_3_i;
 		ts_down[w_ptr_down+3'h2] <= cnt8_down_0_i;
-		w_ptr_down <= (w_ptr_down + 3'h3) % 8;
+		w_ptr_down <= (w_ptr_down + 3'h3) % 4'h8;
 	     end
 	     else if ( valid_down_i == 4'b1011 ) begin
 		ts_down[w_ptr_down] <= cnt8_down_3_i;
@@ -229,26 +236,62 @@
 		  ts_down[w_ptr_down+3'h1] <= cnt8_down_1_i;
 		  ts_down[w_ptr_down+3'h2] <= cnt8_down_2_i;
 		  ts_down[w_ptr_down+3'h3] <= cnt8_down_3_i;
-		  w_ptr_down <= (w_ptr_down + 3'h4) % 8;
+		  w_ptr_down <= (w_ptr_down + 3'h4) % 4'h8;
 	       end
       end // else: !if( rstb == 1'b0 )
    end // always @ (posedge clk, negedge rstb )
 
+   wire LI_active;
+   wire LI_start;
+   wire LI_end;
+
+   wire [7:0] triggered;
+   
+   
    // Instantiate analog memory core
    amem_core #(.DEPTH(8)) amem_core_tb (
       .clk(clk),
       .rstb(rstb),
       .trigger_i(1'b0),
-      .valid_cells_i(8'b11111111),   
-      .LI_valid_i(1'b0),     
+      .valid_cells_i(triggered),   
+      .LI_valid_i( LI_active ),     
       .triggered_mode_i(1'b0),
       .wr_ptr_i(w_ptr_down),
       .circular_en_i(1'b1),
       .WE_time_i(WE_time_i),
       .adc_done_i(1'b0),
-      .read_next_i(1'b0)
+      .read_next_i(1'b0),
+      .read_en_o( read_en_o ),
+      .event_mux_o( event_mux_o ),
+      .sample_ready_o( sample_ready_o ),
+      .amem_empty_o( amem_empty_o ),
+      .amem_full_o( amem_full_o )
    );   
 
+   // Instantiate LI controller
+   LI_control LI_cont (
+		       .valid_up_i( valid_up_i ),
+		       .LI_length_i( 8'hA ),     // XXX -- from register
+		       .LI_start_o( LI_start ),
+		       .LI_end_o( LI_end ),
+		       .LI_active_o( LI_active ),
+		       .clk( clk ),
+		       .rstb( rstb )
+		       );
+
+   trig_cont trigger (
+		      .w_ptr_i( w_ptr_up ),
+		      .LI_start_i( LI_start ),
+		      .LI_end_i( LI_end ),
+		      .trigger_i( trigger_i ),
+		      .read_en_i( read_en_o ),
+		      .event_mux_i( event_mux_o ),
+		      .trigd_o( triggered ),
+		      .clk( clk ),
+		      .rstb( rstb )
+		      );
+   
+   
 endmodule // controller
 
 
