e31bd285e938a93a621dd2096cbdbb55333e7993
David Drobner <david@drobner.ca>
2026-07-06 16:09:00 -0400

    Update APP tb

--- asic/verif/APP_tb.v
+++ asic/verif/APP_tb.v
@@ -17,51 +17,68 @@ module APP_tb;
     reg vcomp = 0; // from tb
     wire rst_init;
     assign rst_init = !rstb;
+    
+    // LI Control signals
+    wire [3:0] LI_valid_up_o;
+    wire [7:0] LI_length_o;
+    wire LI_start_i;
+    wire LI_end_i;
+    wire LI_active_i;
 
     // analog interface to amem_core
-    analog_if analog_if_tb (
-        .clk(clk),
-        .rstb(rstb),
-        .cmp_i(vcomp),
-        .clk_cnt_i(timestamp[7:0]),
-	    .cnt8_up_0_o(cnt8_up_0_o),
-	    .cnt8_up_1_o(cnt8_up_1_o),
-	    .cnt8_up_2_o(cnt8_up_2_o),
-	    .cnt8_up_3_o(cnt8_up_3_o),
-	    .cnt8_down_0_o(cnt8_down_0_o),
-	    .cnt8_down_1_o(cnt8_down_1_o),
-	    .cnt8_down_2_o(cnt8_down_2_o),
-	    .cnt8_down_3_o(cnt8_down_3_o),
-        .valid_up_o(valid_up_o),
-        .valid_down_o(valid_down_o)
-    );
-
-    // amem_core lives inside controller
-    controller controller_tb (
-        .clk(clk),
-        .rstb(rstb),
-        .clk_cnt_i(timestamp[7:0]),
-	    .valid_up_i(valid_up_o),
-	    .cnt8_up_0_i(cnt8_up_0_o),
-	    .cnt8_up_1_i(cnt8_up_1_o),
-	    .cnt8_up_2_i(cnt8_up_2_o),
-	    .cnt8_up_3_i(cnt8_up_3_o),
-	    .valid_down_i(valid_down_o),
-	    .cnt8_down_0_i(cnt8_down_0_o),
-	    .cnt8_down_1_i(cnt8_down_1_o),
-	    .cnt8_down_2_i(cnt8_down_2_o),
-	    .cnt8_down_3_i(cnt8_down_3_o),
-	    .WE_ampl_i(), 
-	    .WE_time_i(WE_time_i)
-    );
+     analog_if analog_if_tb (
+         .clk(clk),
+         .rstb(rstb),
+         .cmp_i(vcomp),
+         .clk_cnt_i(timestamp[7:0]),
+ 	    .cnt8_up_0_o(cnt8_up_0_o),
+ 	    .cnt8_up_1_o(cnt8_up_1_o),
+ 	    .cnt8_up_2_o(cnt8_up_2_o),
+ 	    .cnt8_up_3_o(cnt8_up_3_o),
+ 	    .cnt8_down_0_o(cnt8_down_0_o),
+ 	    .cnt8_down_1_o(cnt8_down_1_o),
+ 	    .cnt8_down_2_o(cnt8_down_2_o),
+ 	    .cnt8_down_3_o(cnt8_down_3_o),
+         .valid_up_o(valid_up_o),
+         .valid_down_o(valid_down_o)
+     );
+ 
+     // amem_core lives inside controller
+     controller controller_tb (
+         .clk(clk),
+         .rstb(rstb),
+         .clk_cnt_i(timestamp[7:0]),
+ 	    .valid_up_i(valid_up_o),
+ 	    .cnt8_up_0_i(cnt8_up_0_o),
+ 	    .cnt8_up_1_i(cnt8_up_1_o),
+ 	    .cnt8_up_2_i(cnt8_up_2_o),
+ 	    .cnt8_up_3_i(cnt8_up_3_o),
+ 	    .valid_down_i(valid_down_o),
+ 	    .cnt8_down_0_i(cnt8_down_0_o),
+ 	    .cnt8_down_1_i(cnt8_down_1_o),
+ 	    .cnt8_down_2_i(cnt8_down_2_o),
+ 	    .cnt8_down_3_i(cnt8_down_3_o),
+ 	    //.WE_ampl_i(), 
+ 	    .WE_time_i(WE_time_i)
+     );
 
     // Instantiate app 1ch behavioral model
     // XXX - needs update based on Ravi's simulations,
     // then we can generate WE here, instead of APP_cocotb.py
     app_1ch_behav app_1ch_tb (
         .clk(clk),
-        .rst_init(rst_init),
+       .rst_init(rst_init),
         .vcomp(vcomp)
+    );
+
+    LI_control li_control_tb (
+        .valid_up_i(LI_valid_up_o),
+        .LI_length_i(LI_length_o),
+        .LI_start_o(LI_start_i),
+        .LI_end_o(LI_end_i),
+        .LI_active_o(LI_active_i),
+        .clk(clk),
+        .rstb(rstb)
     );
 
     always #10 clk = ~clk; // 50MHz clock
@@ -69,6 +86,8 @@ module APP_tb;
 
     initial begin
         #0 rstb = 1'b0;
+        $shm_open("waves.shm");
+        $shm_probe("AS");
     end
 
 endmodule
